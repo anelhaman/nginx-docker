e092d9349d5364b76826ce1bff068343d56db1bb
rachen <rachen@central.tech>
2018-09-12 22:45:26 +0700

    make idem

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
-docker run -d --rm --name nginx \
+docker run -d \
+--rm --name nginx \
 -p 80:80 \
 -v $(PWD)/default.conf:/etc/nginx/conf.d/default.conf:ro \
--v $(PWD)/nginx.conf:/etc/nginx/nginx.conf:ro \
+-v $(PWD)/nginx.conf:/etc/nginx/nginx.conf \
 -v $(pwd)/cache/:/tmp/cache/ \
 -v $(pwd)/logs/:/var/log/nginx/  \
 nginx:latest
