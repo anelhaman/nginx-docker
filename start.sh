f5f116e4a2ec4dd5f0ecdda47acd68ac102a3d46
rachen <rachen@central.tech>
2018-09-12 22:32:30 +0700

    add custom main nginx config

--- start.sh
+++ start.sh
@@ -1,1 +1,7 @@
-docker run -d --rm --name nginx -p 80:80 -v $(PWD)/default.conf:/etc/nginx/conf.d/default.conf:ro -v $(pwd)/cache/:/tmp/cache/ -v $(pwd)/logs/:/var/log/nginx/  nginx:latest
+docker run -d --rm --name nginx \
+-p 80:80 \
+-v $(PWD)/default.conf:/etc/nginx/conf.d/default.conf:ro \
+-v $(PWD)/nginx.conf:/etc/nginx/nginx.conf:ro \
+-v $(pwd)/cache/:/tmp/cache/ \
+-v $(pwd)/logs/:/var/log/nginx/  \
+nginx:latest
