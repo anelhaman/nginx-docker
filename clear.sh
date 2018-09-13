482be7f20488cff1a596be2d5344125ac74d22cf
rachen <rachen@central.tech>
2018-09-13 13:53:16 +0700

    add upstream config cache active 1m

--- clear.sh
+++ clear.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 docker container stop nginx
+docker container rm nginx
 rm -rf cache/* logs/*
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 docker run -d \
---rm --name nginx \
+--name nginx \
 -p 80:80 \
 -v $(PWD)/default.conf:/etc/nginx/conf.d/default.conf:ro \
 -v $(PWD)/nginx.conf:/etc/nginx/nginx.conf \
