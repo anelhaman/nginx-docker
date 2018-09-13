11bab0c2e18d3230c74ea70c339a97f33d6af4fe
rachen <rachen@central.tech>
2018-09-13 20:36:04 +0700

    cache, ignore header

--- create-backend.sh
+++ create-backend.sh
@@ -3,4 +3,7 @@
 docker run -d --name web prch12/alpineweb:latest
 docker run -d --name web-backup prch12/alpineweb:web2
 
+docker run -d --rm --name jsonserver -v $(PWD)/db.json:/data/db.json clue/json-server
+docker container stop jsonserver
+
 mkdir cache logs
